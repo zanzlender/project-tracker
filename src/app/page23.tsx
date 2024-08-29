e751311e8d0b694103d88b4cea4acbd098d78033
zanzlender <a1zan.zlender@gmail.com>
2024-08-29 23:21:00 +0200

    fix build errors

--- src/app/page23.tsx
+++ src/app/page23.tsx
@@ -4,9 +4,9 @@ import { LatestPost } from "~/app/_components/post";
 import { api, HydrateClient } from "~/trpc/server";
 
 export default async function Home() {
-  const hello = await api.post.hello({ text: "from tRPC" });
+  /* const hello = await api.post.hello({ text: "from tRPC" });
 
-  void api.post.getLatest.prefetch();
+  void api.post.getLatest.prefetch(); */
 
   return (
     <HydrateClient>
@@ -41,7 +41,7 @@ export default async function Home() {
           </div>
           <div className="flex flex-col items-center gap-2">
             <p className="text-2xl text-white">
-              {hello ? hello.greeting : "Loading tRPC query..."}
+              {/* {hello ? hello.greeting : "Loading tRPC query..."} */}
             </p>
           </div>
 
