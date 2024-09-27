8aa06a465e0259bf321fb6617bddf8bd3188e811
zanzlender <a1zan.zlender@gmail.com>
2024-09-27 16:59:54 +0200

    add middleware matcher

--- src/middleware.ts
+++ src/middleware.ts
@@ -41,7 +41,8 @@ export default clerkMiddleware(async (auth, request, event) => {
 export const config = {
   matcher: [
     // Skip Next.js internals and all static files, unless found in search params
-    "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
+    // and /monitoring for Vercel Speed insigts
+    "/((?!_next|monitoring|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
     // Always run for API routes
     "/(api|trpc)(.*)",
   ],
